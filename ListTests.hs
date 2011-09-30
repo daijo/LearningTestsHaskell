70e91a13bd4a4b3f63f4c9b0a1240ea2266a032f
Daniel Hjort <daniel.hjort@gmail.com>
2011-09-30 15:52:56 +0700

    Added test cases.

--- ListTests.hs
+++ ListTests.hs
@@ -24,5 +24,11 @@ testGetLast = TestCase (assertEqual "" (last [1..25]) 25)
 
 testGetInit = TestCase (assertEqual "" (init [1..25]) [1..24])
 
-listTests = TestList [TestLabel "testAppend" testAppend, TestLabel "testConcat" testConcat, TestLabel "testGetElement" testGetElement, TestLabel "testGetElement2" testGetElement2, TestLabel "testCompare" testCompare, TestLabel "testCompare2" testCompare2, TestLabel "testGetHead" testGetHead, TestLabel "testGetTail" testGetTail, TestLabel "testGetLast" testGetLast, TestLabel "testGetInit" testGetInit]
+testGetLength = TestCase (assertEqual "" (length [1..10]) 10)
+
+testReverse = TestCase (assertEqual "" (reverse [1..10]) [10,9..1])
+
+testNull = TestCase (assertBool "" (null []))
+
+listTests = TestList [TestLabel "testAppend" testAppend, TestLabel "testConcat" testConcat, TestLabel "testGetElement" testGetElement, TestLabel "testGetElement2" testGetElement2, TestLabel "testCompare" testCompare, TestLabel "testCompare2" testCompare2, TestLabel "testGetHead" testGetHead, TestLabel "testGetTail" testGetTail, TestLabel "testGetLast" testGetLast, TestLabel "testGetInit" testGetInit, TestLabel "testGetLength" testGetLength, TestLabel "testReverse" testReverse, TestLabel "testNull" testNull]
 
