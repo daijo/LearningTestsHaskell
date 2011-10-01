fb81f6efc8a77ae9dbae2bc841cf346b27fcb534
Daniel Hjort <daniel.hjort@gmail.com>
2011-10-01 15:48:55 +0700

    New test cases, fixed old (expected, actual order).

--- ListTests.hs
+++ ListTests.hs
@@ -4,31 +4,43 @@
 
 import HUnit
 
-testAppend = TestCase (assertEqual "" ('A':"BC") "ABC")
+testAppend = TestCase (assertEqual "" "ABC" ('A':"BC"))
 
-testConcat = TestCase (assertEqual "" ([1,2] ++ [3,4]) [1..4])
+testConcat = TestCase (assertEqual "" [1..4] ([1,2] ++ [3,4]))
 
-testGetElement = TestCase (assertEqual "" ("Blue Pumpkin" !! 5) 'P')
+testGetElement = TestCase (assertEqual "" 'P' ("Blue Pumpkin" !! 5))
 
-testGetElement2 = TestCase (assertEqual "" ([1..] !! 5) 6)
+testGetElement2 = TestCase (assertEqual "" 6 ([1..] !! 5))
 
 testCompare = TestCase (assertBool "" ([1,2,3]<[3,2,1]))
 
 testCompare2 = TestCase (assertBool "" ([1,2,3]==[1,2,3]))
 
-testGetHead = TestCase (assertEqual "" (head [4..20]) 4)
+testGetHead = TestCase (assertEqual "" 4 (head [4..20]))
 
-testGetTail = TestCase (assertEqual "" (tail [4..20]) [5..20])
+testGetTail = TestCase (assertEqual "" [5..20]  (tail [4..20]) )
 
-testGetLast = TestCase (assertEqual "" (last [1..25]) 25)
+testGetLast = TestCase (assertEqual "" 25 (last [1..25]))
 
-testGetInit = TestCase (assertEqual "" (init [1..25]) [1..24])
+testGetInit = TestCase (assertEqual "" [1..24] (init [1..25]))
 
-testGetLength = TestCase (assertEqual "" (length [1..10]) 10)
+testGetLength = TestCase (assertEqual "" 10 (length [1..10]))
 
-testReverse = TestCase (assertEqual "" (reverse [1..10]) [10,9..1])
+testReverse = TestCase (assertEqual "" [10,9..1]  (reverse [1..10]))
 
 testNull = TestCase (assertBool "" (null []))
 
-listTests = TestList [TestLabel "testAppend" testAppend, TestLabel "testConcat" testConcat, TestLabel "testGetElement" testGetElement, TestLabel "testGetElement2" testGetElement2, TestLabel "testCompare" testCompare, TestLabel "testCompare2" testCompare2, TestLabel "testGetHead" testGetHead, TestLabel "testGetTail" testGetTail, TestLabel "testGetLast" testGetLast, TestLabel "testGetInit" testGetInit, TestLabel "testGetLength" testGetLength, TestLabel "testReverse" testReverse, TestLabel "testNull" testNull]
+testTake = TestCase (assertEqual "" "Dan" (take 3 "Daniel"))
+
+testDrop = TestCase (assertEqual "" "Truck" (drop 8 "Monster Truck"))
+
+testMaximum = TestCase (assertEqual "" 6 (maximum [1,3,5,6,2]))
+
+testMinimum = TestCase (assertEqual "" 'C' (minimum "Chenchen"))
+
+testSum = TestCase (assertEqual "" 10 (sum [1,2,3,4]))
+
+testProduct = TestCase (assertEqual "" 24 (product [1,2,3,4]))
+
+listTests = TestList [TestLabel "testAppend" testAppend, TestLabel "testConcat" testConcat, TestLabel "testGetElement" testGetElement, TestLabel "testGetElement2" testGetElement2, TestLabel "testCompare" testCompare, TestLabel "testCompare2" testCompare2, TestLabel "testGetHead" testGetHead, TestLabel "testGetTail" testGetTail, TestLabel "testGetLast" testGetLast, TestLabel "testGetInit" testGetInit, TestLabel "testGetLength" testGetLength, TestLabel "testReverse" testReverse, TestLabel "testNull" testNull, TestLabel "testTake" testTake, TestLabel "testDrop" testDrop, TestLabel "testMaximum" testMaximum, TestLabel "testMinimum" testMinimum, TestLabel "testSum" testSum, TestLabel "testProduct" testProduct]
 
