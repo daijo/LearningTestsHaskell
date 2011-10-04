49fcf21f6c26ee2625b2e5bbaf4b6e5a44a10502
Daniel Hjort <daniel.hjort@gmail.com>
2011-10-04 21:11:35 +0700

    Added as-pattern test case.

--- FunctionSyntaxTest.hs
+++ FunctionSyntaxTest.hs
@@ -29,6 +29,11 @@ matchTail :: [a] -> [a]
 matchTail [] = error "Empty list!"
 matchTail (_:xs) = xs
 
+secondLetterOf :: String -> String
+secondLetterOf "" = "Empty String!"
+secondLetterOf (x:[]) = "One letter String!"
+secondLetterOf all@(x:y:xs) = "Second letter of " ++ all ++ " is " ++ [y]
+
 -- Test cases
 
 testBasicMatching = TestCase (do assertEqual "" "one" (matchingFunc 1)
@@ -48,4 +53,9 @@ testMatchHead = TestCase (assertEqual "" 1 (matchHead [1, 2, 3]))
 
 testMatchTail = TestCase (assertEqual "" [2, 3] (matchTail [1, 2, 3]))
 
-allTests = TestList [TestLabel "testBasicMatching" testBasicMatching, TestLabel "testFactorial" testFactorial, TestLabel "testScaleVector" testScaleVector, TestLabel "testGenericTriple" testGenericTriple, TestLabel "testMatchHead" testMatchHead, TestLabel "testMatchTail" testMatchTail]
+testSecondLetterOf = TestCase (do assertEqual "" "Empty String!" (secondLetterOf "")
+                                  assertEqual "" "One letter String!" (secondLetterOf "D")
+                                  assertEqual "" "Second letter of Daniel is a" (secondLetterOf "Daniel"))
+
+
+allTests = TestList [TestLabel "testBasicMatching" testBasicMatching, TestLabel "testFactorial" testFactorial, TestLabel "testScaleVector" testScaleVector, TestLabel "testGenericTriple" testGenericTriple, TestLabel "testMatchHead" testMatchHead, TestLabel "testMatchTail" testMatchTail, TestLabel "testSecondLetterOf" testSecondLetterOf]
