c32681e6dd91bccc4b4493360adc38c46c684a05
Daniel Hjort <daniel.hjort@gmail.com>
2011-10-04 19:34:26 +0700

    Added tuple test cases.

--- FunctionSyntaxTest.hs
+++ FunctionSyntaxTest.hs
@@ -15,6 +15,12 @@ factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
+scaleVector :: (Double, Double) -> Double -> (Double, Double)
+scaleVector (x, y) s = (s * x, s * y)
+
+secondInGenericTriple :: (a, b, c) -> b
+secondInGenericTriple (_, x, _) = x
+
 -- Test cases
 
 testBasicMatching = TestCase (do assertEqual "" "one" (matchingFunc 1)
@@ -25,4 +31,9 @@ testFactorial = TestCase (do assertEqual "" 1 (factorial 0)
                              assertEqual "" 120 (factorial 5)
                              assertEqual "" 720 (factorial 6))
 
-allTests = TestList [TestLabel "testBasicMatching" testBasicMatching, TestLabel "testFactorial" testFactorial]
+testScaleVector = TestCase (assertEqual "" (1.5, 1.5) (scaleVector (1, 1) 1.5))
+
+testGenericTriple = TestCase (do assertEqual "" 2 (secondInGenericTriple (1, 2, 3))
+                                 assertEqual "" "er" (secondInGenericTriple ('i', "er", 3)))
+
+allTests = TestList [TestLabel "testBasicMatching" testBasicMatching, TestLabel "testFactorial" testFactorial, TestLabel "testScaleVector" testScaleVector, TestLabel "testGenericTriple" testGenericTriple]
